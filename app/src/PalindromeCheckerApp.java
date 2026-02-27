1f5592fb6342002014b435128553c9722423bd59
madhumalar07 <madhumalar2784@gmail.com>
2026-02-27 16:04:12 +0530

    uc2-hardcorePalindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,10 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public  class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+import java.util.Scanner;
 
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String input = sc.nextLine();
+
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        boolean isPalindrome = input.equalsIgnoreCase(reversed);
+
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        sc.close();
     }
 }
