255786310f75bf25baed56fb9afacc706547c742
madhumalar07 <madhumalar2784@gmail.com>
2026-02-27 14:47:49 +0530

    uc1 - welcome message for palindrome app

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public  class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+
+    }
+}
