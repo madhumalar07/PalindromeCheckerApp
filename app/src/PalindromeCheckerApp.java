59bad4f0c7a3ce765bd00a763e2e34fb7cf5c4c5
madhumalar07 <madhumalar2784@gmail.com>
2026-03-09 13:57:01 +0530

    ucl-reverestring

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+public class UseCase3PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=== Palindrome Checker App ===");
+        System.out.println("Palindrome Checker App");
         System.out.print("Enter a string: ");
 
         String original = sc.nextLine();
         String reversed = "";
 
 
-        for(int i = original.length() - 1; i >= 0; i--) {
+        for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + original.charAt(i);
         }
 
 
-        if(original.equals(reversed)) {
-            System.out.println("Result: The given string is a Palindrome.");
-        }
-        else {
-            System.out.println("Result: The given string is NOT a Palindrome.");
+        if (original.equals(reversed)) {
+            System.out.println("Result: The string is a Palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a Palindrome.");
         }
 
         sc.close();
